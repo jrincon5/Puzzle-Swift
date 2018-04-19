5edd7cb14e332597d0368d93132a1d251f4d39cd
Julián Rincón Usma - Ceiba Software <julian.rincon@Soportes-Mac-mini-2.local>
2018-04-19 07:13:30 -0500

    Agregando funcion random para el puzzle

--- Puzzle/ViewController.swift
+++ Puzzle/ViewController.swift
@@ -21,9 +21,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         makeTiles();
+        randomTiles();
     }
     
     func makeTiles() {
+        self.tileArray = [];
+        self.tileCenterArray = [];
         let boardWidth = Int(self.board.frame.width);
         self.tileWidth = boardWidth / 4;
         self.tileCenterX = self.tileWidth / 2;
@@ -33,27 +36,41 @@ class ViewController: UIViewController {
         for _ in 0..<4 {
             for _ in 0..<4 {
                 let tileFrame : CGRect = CGRect(x: 0, y: 0, width: self.tileWidth - 2, height: self.tileWidth - 2);
-                let tile : UILabel = UILabel(frame: tileFrame);
+                let tile : customLabel = customLabel(frame: tileFrame);
                 let currentCenter : CGPoint = CGPoint(x: self.tileCenterX, y : self.tileCenterY);
                 tile.center = currentCenter;
+                tile.text = "\(tileNumber)";
+                tile.textAlignment = NSTextAlignment.center;
                 self.tileCenterArray.add(currentCenter);
                 tile.backgroundColor = UIColor.red;
                 self.board.addSubview(tile);
                 tileNumber = tileNumber + 1;
-                tile.text = "\(tileNumber)";
                 self.tileArray.add(tile);
                 self.tileCenterX = self.tileCenterX + self.tileWidth;
             }
         self.tileCenterX = self.tileWidth / 2;
         self.tileCenterY = self.tileCenterY + self.tileWidth;
         }
-        let lastTile : UILabel = self.tileArray.lastObject as! UILabel;
+        let lastTile : customLabel = self.tileArray.lastObject as! customLabel;
         lastTile.removeFromSuperview();
         self.tileArray.removeObject(at : 15);
+    }
+    
+    func randomTiles() {
+        for anyTile in self.tileArray {
+            let randomIndex : Int = Int(arc4random()) % self.tileCenterArray.count;
+            let randomCenter : CGPoint = self.tileCenterArray[randomIndex] as! CGPoint;
+            (anyTile as! customLabel).center = randomCenter;
+            self.tileCenterArray.removeObject(at: randomIndex);
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+class customLabel : UILabel {
+    var originCenter : CGPoint = CGPoint(x : 0, y : 0);
 }
